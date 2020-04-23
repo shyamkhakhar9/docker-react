e43c40d6a46e0a1a4c0a7600d8354eb897c738f9
root <root@ip-172-31-20-36.ap-south-1.compute.internal>
2020-04-23 19:18:06 +0000

    minor change in app.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import REACTDOM from 'react-dom';
+import ReactDOM from 'react-dom';
 import App from './App';
 
 it('renders withour crashing', () => {
   const div = document.createElement('div');
-  React.render.render(<App />, div);
-  React.render.unmountComponentAtNode(div);
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
 });
