edd638989bad96077cfe9ba3e14f898d81ccf309
root <root@ip-172-31-20-36.ap-south-1.compute.internal>
2020-04-25 19:36:55 +0000

    updated app.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+import sum from './sum';
+it('sums numbers', () => {
+  expect(sum(1, 2)).toEqual(3);
+  expect(sum(2, 2)).toEqual(4);
 });
+
